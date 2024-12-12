5808eca245796d5014d7fa27ee8af82d3006a42c
TornikeKalmaxelidze <tokokalmaxelidzee@gmail.com>
2024-12-12 15:22:27 +0400

    sagagasgaskgnasokgoasiogasg
    rtuli iko

--- src/main.js
+++ src/main.js
@@ -8,4 +8,50 @@ function getCities() {
         })
     }
     return cities;
+ 
 }
+
+const cities = getCities();
+const citiesTable = document.getElementById('cities-table');
+let sorted = false;
+
+cities.forEach(({ name, population }) => {
+    const tr = document.createElement('tr');
+
+    tr.innerHTML = (`<td>${name}</td>
+    <td>${population}</td>`)
+    citiesTable.querySelector('tbody').appendChild(tr);
+})
+
+const header = citiesTable.querySelectorAll('th')[1];
+const headerIndicator = header.querySelector('span');
+
+console.log(header);
+header.onclick = () => {
+    sorted = !sorted;
+    if (sorted) {
+        headerIndicator.innerText = '▼';
+    } else {
+        headerIndicator.innerText = '▲';
+    }
+    citiesTable.querySelector('tbody').innerHTML = '';
+    cities.sort((c1, c2) => {
+        if (sorted)
+            return c2.population - c1.population;
+        return c1.population - c2.population;
+    })
+        .forEach(({ name, population }) => {
+            const tr = document.createElement('tr');
+
+            tr.innerHTML = (`<td>${name}</td>
+        <td>${population}</td>`)
+            citiesTable.querySelector('tbody').appendChild(tr);
+        })
+      
+}
+// const resetButton = document.getElementById('reset-button');
+// resetButton.onclick = () => {
+//     cities.length = 0; 
+//     cities.push(...originalCities);
+//     renderCities(cities); 
+// };  reset rogor unda ver mixvdi da mainteresebs rogor aris shesadzlebeli
